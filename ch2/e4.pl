290ca732728dba14086ff18a62561fcd386d2d50
Sam Boyer <tech@samboyer.org>
2015-04-30 02:31:40 -0400

    Chapters 1, 2, and part of 3

---
+++ ch2/e4.pl
@@ -0,0 +1,6 @@
+word(astante,  a,s,t,a,n,t,e).
+word(astoria,  a,s,t,o,r,i,a).
+word(baratto,  b,a,r,a,t,t,o).
+word(cobalto,  c,o,b,a,l,t,o).
+word(pistola,  p,i,s,t,o,l,a).
+word(statale,  s,t,a,t,a,l,e).
